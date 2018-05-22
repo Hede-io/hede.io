1fe6d3063c145d2a6fb4ba0e05eac25bf0a0a0db
ercu <ercueser@gmail.com>
2018-05-23 01:30:13 +0300

    # to make inner link

--- src/vendor/steemitHtmlReady.js
+++ src/vendor/steemitHtmlReady.js
@@ -221,7 +221,7 @@ function linkify(content, mutate, hashtags, usertags, images, links) {
     const tagLower = tag2.toLowerCase();
     if (hashtags) hashtags.add(tagLower);
     if (!mutate) return tag;
-    return `${space}<a href="/trending/${tagLower}">${tag}</a>`;
+    return `${space}<a href="/?q=${tagLower}">${tag2}</a>`;
   });
 
   // usertag (mention)
