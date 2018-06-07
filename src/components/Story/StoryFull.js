a91d2114f8efcc92ec8c3529746796c2177a8a3f
ercu <ercueser@gmail.com>
2018-06-07 19:48:30 +0300

    allow mods to change cached version of post on the server

--- src/components/Story/StoryFull.js
+++ src/components/Story/StoryFull.js
@@ -294,7 +294,7 @@ class StoryFull extends React.Component {
 
     let popoverMenu = [];
 
-    if ((isModerator || ownPost) && post.cashout_time !== '1969-12-31T23:59:59') {
+    if (isModerator || (ownPost && post.cashout_time !== '1969-12-31T23:59:59')) {
       popoverMenu = [...popoverMenu, <PopoverMenuItem key="edit">
         {saving ? <Icon type="loading" /> : <i className="iconfont icon-write" />}
         <FormattedMessage id="edit_post" defaultMessage="Edit post" />
