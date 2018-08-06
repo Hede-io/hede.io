697c1f2b947ac353a2209a46a50bef3460e64120
ercu <ercueser@gmail.com>
2018-08-06 18:44:36 +0300

    update and simplify rules

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -24,9 +24,8 @@ export const Rules = ({type, acceptRules, inEditor}) => {
         <h2><CategoryIcon from="from-rules"  type="entry"/> Entry Rules</h2>
         {inEditor ? <p><small><a href="https://hede.io/rules" target="_blank"></a></small></p> : null}
         <ul>
-          <li>✅ Answer the question "What/Who?" by describing the topic or by giving examples for the topic.</li>
-          <li>✅ Description in the first sentence is highly advised but not required if it was done before.</li>
-          <li>✅ Please share your knowledge only and directly about the topic. You can link sub-topics to create content.</li>
+          <li>✅ Please describe the topic and answer "what is it?" first.</li>
+          <li>✅ Please share your personal knowledge directly about the topic. You can always link to sub-topics.</li>
           <li>✅ Write for a large audience. Lots of people will read this so give background information.</li>
           <li>🚫 Don't duplicate another entry's content if you will not add more value into it.</li>
           <li>🚫 Don't respond or comment to another user's entry in your's. To do so, open that entry and write your comment under it.</li>
